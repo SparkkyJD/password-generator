51a9e2fa7282357a0f2716dc7abc037b773c89ff
SparkkyJD <127361245+SparkkyJD@users.noreply.github.com>
2023-05-29 16:48:05 -0400

    Update script.js
    added correct code

--- Assets/script.js
+++ Assets/script.js
@@ -1,95 +1,60 @@
-var generateBtn = document.querySelector("#generate");
-var generatePassword;
-var promptNumber;
-var promptCharacter;
-var promptUppercase;
-var promptLowercase;
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",",
-             "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[",
-             "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-         "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-space = [];
-var choices;
-var toUpper = function (x) {
-    return x.toUpperCase();
-};
-alpha2 = alpha.map(toUpper);
-generateBtn.addEventListener("click", function () {
-    ps = generatePassword();
-    placeholder = ps;
+// event listener for generate btn
+const generateBtn = document.querySelector('#generate');
+const passwordInput = document.querySelector('#password');
+generateBtn.addEventListener('click', () => {
+  passwordInput.value = generatePassword();
 });
 function generatePassword() {
-  generatePassword = parseInt(prompt("How many characters? (Choose between 8 and 128)"));
-  if (!generatePassword) {
-      alert("This needs a value");
-  } else if (generatePassword < 8 || generatePassword > 128) {   
-    generatePassword = parseInt(prompt("You must choose between 8 and 128"));
-  } else {
-      promptUppercase = confirm("Will this password have uppercase letters?");
-      promptLowercase = confirm("Will this password have lowercase letters?");
-      promptNumber = confirm("Will this password have numbers?");
-      promptCharacter = confirm("Will this password have special characters?");
-  };
-  if (!promptCharacter && !promptNumber && !promptUppercase && !promptLowercase) {
-      choices = alert("You must choose a criteria!");
+  // string of characters available for the function
+  const lowercaseOption = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseOption = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numericOption = '0123456789';
+  const specialOption = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+  //prompts user for password length  
+  let passwordLength = prompt('Choose a password length between 8-128 characters:');
+  // throw error if criteria isnt met
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt('Invalid password length. Enter a length between 8-128:');
   }
-  else if (promptCharacter && promptNumber && promptUppercase && promptLowercase) {
-      choices = character.concat(number, alpha, alpha2);
-  }
-  else if (promptCharacter && promptNumber && promptUppercase) {
-      choices = character.concat(number, alpha2);
-  }
-  else if (promptCharacter && promptNumber && promptLowercase) {
-      choices = character.concat(number, alpha);
-  }
-  else if (promptCharacter && promptLowercase && promptUppercase) {
-      choices = character.concat(alpha, alpha2);
-  }
-  else if (promptNumber && promptLowercase && promptUppercase) {
-      choices = number.concat(alpha, alpha2);
-  }
-  else if (promptCharacter && promptNumber) {
-      choices = character.concat(number);
-  } 
-  else if (promptCharacter && promptLowercase) {
-      choices = character.concat(alpha);
-  } 
-  else if (promptCharacter && promptUppercase) {
-      choices = character.concat(alpha2);
-  }
-  else if (promptLowercase && promptNumber) {
-      choices = alpha.concat(number);
-  } 
-  else if (promptLowercase && promptUppercase) {
-      choices = alpha.concat(alpha2);
-  } 
-  else if (promptNumber && promptUppercase) {
-      choices = number.concat(alpha2);
-  }
-  else if (promptCharacter) {
-      choices = character;
-  }
-  else if (promptNumber) {
-      choices = number;
-  }
-  else if (promptLowercase) {
-      choices = alpha;
-  }
-  else if (promptUppercase) {
-      choices = space.concat(alpha2);
-  };
-  var password = [];
-  for (var i = 0; i < generatePassword; i++) {
-      var pickChoices = choices[Math.floor(Math.random() * choices.length)];
-      password.push(pickChoices);
-  }
-  var ps = password.join("");
-  UserInput(ps);
-  return ps;
-}
-function UserInput(ps) {
-  document.getElementById("password").textContent = ps;
-}
+  // prompts user for all other criteria
+  let LowercasePrompt = confirm('Include lowercase characters?');
+  let UppercasePrompt = confirm('Include uppercase characters?');
+  let NumericPrompt = confirm('Include numeric characters?');
+  let SpecialPrompt = confirm('Include special characters?');
 
+  if (
+    !LowercasePrompt &&
+    !UppercasePrompt &&
+    !NumericPrompt &&
+    !SpecialPrompt
+  ) {
+    // throws error if user inputs nothing 
+    alert('Please select at least one character type.');
+    return '';
+  }
+  // stores available characters and generated password
+  let chars = '';
+  let password = '';
+  // logic for all variables
+  if (LowercasePrompt) {
+    chars += lowercaseOption;
+    password += lowercaseOption[Math.floor(Math.random() * lowercaseOption.length)];
+  }
+  if (UppercasePrompt) {
+    chars += uppercaseOption;
+    password += uppercaseOption[Math.floor(Math.random() * uppercaseOption.length)];
+  }
+  if (NumericPrompt) {
+    chars += numericOption;
+    password += numericOption[Math.floor(Math.random() * numericOption.length)];
+  }
+  if (SpecialPrompt) {
+    chars += specialOption;
+    password += specialOption[Math.floor(Math.random() * specialOption.length)];
+  }
+  for (let i = password.length; i < passwordLength; i++) {
+    password += chars[Math.floor(Math.random() * chars.length)];
+  }
+  // returns generated password 
+  return password;
+}
